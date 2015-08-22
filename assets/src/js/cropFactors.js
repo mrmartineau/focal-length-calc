c0629ee2a03b5bd6c64888b3e2844b2b843daf19
Zander Martineau <zander@martineau.tv>
2015-08-22 12:12:14 +0100

    move crop factors into external file

---
+++ assets/src/js/cropFactors.js
@@ -0,0 +1,122 @@
+module.exports = [
+	{
+		_id: 0,
+		name: '1/3"',
+		description: 'security cameras',
+		height: 4.29,
+		cropFactor: 7.21,
+		checked: false
+	},
+	{
+		_id: 1,
+		name: '1/2.5"',
+		description: '(many superzoom and point-and-shoot cameras)',
+		height: 4.29,
+		cropFactor: 5.6,
+		checked: false
+	},
+	{
+		_id: 2,
+		name: '1/2.3"',
+		description: '(compacts and superzooms like Canon Powershot SX series)',
+		height: 4.62,
+		cropFactor: 5.2,
+		checked: false
+	},
+	{
+		_id: 3,
+		name: '1/1.8"',
+		description: '(high-end compacts like Canon Powershot G1 - G7)',
+		height: 5.32,
+		cropFactor: 4.5,
+		checked: false
+	},
+	{
+		_id: 4,
+		name: '1/1.7"',
+		description: '1/1.7"',
+		height: 5.7,
+		cropFactor: 4.2,
+		checked: false
+	},
+	{
+		_id: 5,
+		name: '2/3"',
+		description: '(Fujifilm X10, Fujifilm X20, Sony F828, Sony F717)',
+		height: 6.6,
+		cropFactor: 3.6,
+		checked: false
+	},
+	{
+		_id: 6,
+		name: '1"',
+		description: '(Nikon 1/CX / Sony RX100-series / Sony RX10)',
+		height: 8.8,
+		cropFactor: 2.7,
+		checked: false
+	},
+	{
+		_id: 7,
+		name: '4/3"',
+		description: 'Four Thirds, Micro Four Thirds (used by Olympus and Panasonic for DSLR and MILC respectively)',
+		height: 13,
+		cropFactor: 2,
+		checked: false
+	},
+	{
+		_id: 8,
+		name: 'Sigma Foveon X3',
+		description: '(old sensor pre Merrill cameras)',
+		height: 13.8,
+		cropFactor: 1.7,
+		checked: false
+	},
+	{
+		_id: 9,
+		name: 'Canon APS-C',
+		description: 'Canon APS-C',
+		height: 14.8,
+		cropFactor: 1.6,
+		checked: false
+	},
+	{
+		_id: 10,
+		name: 'APS-C',
+		description: 'General APS-C (Sigma Foveon X3, Fujifilm X-mount, Nikon DX, Pentax K, Ricoh GXR, & Ricoh GR, Samsung NX, Sony α DT & E-Mount (NEX))',
+		height: 15.6,
+		cropFactor: 1.5,
+		checked: false
+	},
+	{
+		_id: 11,
+		name: 'APS-H',
+		description: '(Canon, Leica M8)',
+		height: 18.6,
+		cropFactor: 1.3,
+		checked: false
+	},
+	{
+		_id: 12,
+		name: '35mm',
+		description: '35mm full frame (Canon EF, Leica M9, Nikon FX, Sony α, FE-Mount, Sony RX1)',
+		height: 24,
+		cropFactor: 1,
+		checked: true
+	},
+	{
+		_id: 13,
+		name: 'Medium format',
+		description: '(Hasselblad, Leaf, Phase One, Pentax 645D)',
+		height: 33,
+		cropFactor: 0.79,
+		checked: false
+	},
+	{
+		_id: 14,
+		name: 'Medium format (Hasselblad, Leaf, Phase One)',
+		description: 'Medium format (Hasselblad, Leaf, Phase One)',
+		height: 40.4,
+		cropFactor: 0.64,
+		checked: false
+	}
+];
