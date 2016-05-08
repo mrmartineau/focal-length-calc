485f63af785b0e84387f7508d5ff4d4b8c9384e3
Zander Martineau <zander@martineau.tv>
2016-05-08 10:34:12 +0100

    improve react

--- assets/src/js/react/app.js
+++ assets/src/js/react/app.js
@@ -2,15 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 // import { Router, Route, Link } from 'react-router';
 
-import App from './containers/App';
-
-// TODO:
-// - add react-router: https://github.com/reactjs/react-router
-// - update results based on route
-// - route /0/65 - /0 = activeFactorItem, /65 = focalLength
-// - improve design and styling
+import Home from './containers/Home';
 
 ReactDOM.render(
-	<App />,
+	<Home />,
 	document.getElementById('root')
 )
