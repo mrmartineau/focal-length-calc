80ce6e06ac4aa084ceec6dcd795c89269db04167
Zander Martineau <zander@martineau.tv>
2016-05-11 00:16:19 +0100

    move the description of the crop factor outside and have it update when the select is changed

--- assets/src/js/react/components/cropFactorList.js
+++ assets/src/js/react/components/cropFactorList.js
@@ -13,18 +13,23 @@ const CropFactorList = React.createClass({
 
 	render() {
 		const cropFactorItems = this.props.factors.map(function(factor, index) {
-			return <option key={index} value={index}>{factor.name} - {factor.description}</option>
+			return <option key={index} value={index}>{factor.name}</option>
 		});
 
+		let desc = this.props.factors[this.props.activeCropFactorItem].description;
+
 		return (
-			<div>
-				<h2>Crop Factors</h2>
+			<div className="cropFactors">
+
+				<label>Crop Factors</label>
+				<br/>
 				<select
 					onChange={this.handleChange}
 					value={this.props.activeCropFactorItem}
 				>
 					{cropFactorItems}
 				</select>
+				<div>{desc}</div>
 			</div>
 		);
 	}
