44ffe9771dc1b6a26d0cd940647be372acddfa31
Zander Martineau <zander@martineau.tv>
2016-05-11 00:08:59 +0100

    add header from martineau.tv site

--- assets/src/js/react/containers/Home.js
+++ assets/src/js/react/containers/Home.js
@@ -25,24 +25,37 @@ const Home = React.createClass({
 	},
 
 	render() {
+		var inlineStyles = {
+			'marginTop': '50px'
+		};
+
 		return (
 			<div>
-				<h1>Lens focal length calculator</h1>
-				<p>Have you ever wondered if a 35mm lens is on a micro 4/3 camera, what is the new focal length? This calculates the equivalent focal length and magnification of your lenses.</p>
-				<CropFactorList
-					handleFactorChange={this.handleFactorChange}
-					factors={cropFactors}
-					activeCropFactorItem={this.state.activeCropFactorItem}
-				/>
-				<FocalLengthInput
-					handleFocalLengthChange={this.handleFocalLengthChange}
-					focalLength={this.state.focalLength}
-				/>
-				<Result
-					factors={cropFactors}
-					activeCropFactorItem={this.state.activeCropFactorItem}
-					focalLength={this.state.focalLength}
-				/>
+				<header className="header">
+					<div className="header-inner l-container">
+						<h1 className="header-title p-name" itemprop="headline">Lens focal length calculator</h1>
+						<div className="header-copy">
+							<p>Have you ever wondered if a 35mm lens is on a micro 4/3 camera, what is the new focal length? This calculates the equivalent focal length and magnification of your lenses.</p>
+						</div>
+					</div>
+				</header>
+
+				<div className="l-container" style={inlineStyles}>
+					<FocalLengthInput
+						handleFocalLengthChange={this.handleFocalLengthChange}
+						focalLength={this.state.focalLength}
+					/>
+					<CropFactorList
+						handleFactorChange={this.handleFactorChange}
+						factors={cropFactors}
+						activeCropFactorItem={this.state.activeCropFactorItem}
+					/>
+					<Result
+						factors={cropFactors}
+						activeCropFactorItem={this.state.activeCropFactorItem}
+						focalLength={this.state.focalLength}
+					/>
+				</div>
 			</div>
 		);
 	}
